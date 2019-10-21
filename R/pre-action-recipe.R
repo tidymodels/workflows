1ceca823ad4832e42a2afefd0c1486b729c4b871
DavisVaughan <davis@rstudio.com>
2019-10-21 08:24:42 -0400

    Remove reprep option for now

--- R/pre-action-recipe.R
+++ R/pre-action-recipe.R
@@ -1,27 +1,18 @@
 #' @export
-add_recipe <- function(x, recipe, reprep = TRUE) {
-  action <- new_action_recipe(recipe, reprep)
+add_recipe <- function(x, recipe) {
+  action <- new_action_recipe(recipe)
   add_action(x, action, "recipe")
 }
 
 # ------------------------------------------------------------------------------
 
 fit.action_recipe <- function(object, director) {
-
-  # TODO - How to correctly handle `reprep = FALSE`? What do we put in the
-  # mold slot?
-
-  if (!object$reprep) {
-    abort("`reprep = FALSE` is not yet supported.")
-    #return(director)
-  }
-
   recipe <- object$recipe
   data <- director$data
 
   mold <- hardhat::mold(recipe, data)
 
-  new_action <- new_action_recipe(recipe = recipe, reprep = TRUE, mold = mold, done = TRUE)
+  new_action <- new_action_recipe(recipe = recipe, mold = mold, done = TRUE)
 
   director$workflow$pre$actions$recipe <- new_action
 
@@ -42,14 +33,12 @@ check_conflicts.action_recipe <- function(action, x) {
 
 # ------------------------------------------------------------------------------
 
-new_action_recipe <- function(recipe, reprep, mold = NULL, done = FALSE) {
+new_action_recipe <- function(recipe, mold = NULL, done = FALSE) {
   if (!is_recipe(recipe)) {
     abort("`recipe` must be a recipe.")
   }
 
-  vec_assert(reprep, ptype = logical(), size = 1L)
-
-  new_action_pre(recipe = recipe, reprep = reprep, mold = mold, done = done, subclass = "action_recipe")
+  new_action_pre(recipe = recipe, mold = mold, done = done, subclass = "action_recipe")
 }
 
 is_recipe <- function(x) {

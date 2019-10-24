fae273077da590a70861e97e19ba857a65ab42f9
DavisVaughan <davis@rstudio.com>
2019-10-24 11:49:02 -0400

    Document `ctrl_workflow()`

--- R/control.R
+++ R/control.R
@@ -1,5 +1,11 @@
 # TODO - What else?
 
+#' Control object for a workflow
+#'
+#' `ctrl_workflow()` holds the control parameters for a workflow.
+#'
+#' @param parsnip A parsnip control object.
+#'
 #' @export
 ctrl_workflow <- function(parsnip = fit_control()) {
   data <- list(

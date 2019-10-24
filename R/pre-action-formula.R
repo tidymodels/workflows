16d6abd02db6a9f57cc2de136c82549e68d61feb
DavisVaughan <davis@rstudio.com>
2019-10-24 11:42:12 -0400

    Document `add_formula()`

--- R/pre-action-formula.R
+++ R/pre-action-formula.R
@@ -1,3 +1,17 @@
+#' Add formula terms to a workflow
+#'
+#' `add_formula()` specifies the terms of the model through the usage of a
+#' formula.
+#'
+#' To fit a workflow, one of `add_formula()` or `add_recipe()` _must_ be
+#' specified, but not both.
+#'
+#' @param x A workflow.
+#'
+#' @param formula A formula specifying the terms of the model. It is advised to
+#' not do preprocessing in the formula, and instead use a recipe if that is
+#' required.
+#'
 #' @export
 add_formula <- function(x, formula) {
   action <- new_action_formula(formula)

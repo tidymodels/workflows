1499182444a6f64be1d4439fef192d52f1b77f5c
DavisVaughan <davis@rstudio.com>
2019-11-11 15:22:56 -0500

    Add basic `fit()` tests

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -1,3 +1,41 @@
+test_that("can `fit()` a workflow with a recipe", {
+  rec <- recipes::recipe(mpg ~ cyl, mtcars)
+
+  mod <- parsnip::linear_reg()
+  mod <- parsnip::set_engine(mod, "lm")
+
+  workflow <- workflow()
+  workflow <- add_recipe(workflow, rec)
+  workflow <- add_model(workflow, mod)
+
+  result <- fit(workflow, mtcars)
+
+  expect_is(result$fit$fit, "model_fit")
+
+  expect_equal(
+    coef(result$fit$fit$fit),
+    coef(lm(formula = mpg ~ cyl, data = mtcars))
+  )
+})
+
+test_that("can `fit()` a workflow with a formula", {
+  mod <- parsnip::linear_reg()
+  mod <- parsnip::set_engine(mod, "lm")
+
+  workflow <- workflow()
+  workflow <- add_formula(workflow, mpg ~ cyl)
+  workflow <- add_model(workflow, mod)
+
+  result <- fit(workflow, mtcars)
+
+  expect_is(result$fit$fit, "model_fit")
+
+  expect_equal(
+    coef(result$fit$fit$fit),
+    coef(lm(formula = mpg ~ cyl, data = mtcars))
+  )
+})
+
 test_that("missing `data` argument has a nice error", {
   mod <- parsnip::linear_reg()
   mod <- parsnip::set_engine(mod, "lm")
@@ -18,3 +56,11 @@ test_that("cannot fit without a pre stage", {
 
   expect_error(fit(workflow, mtcars), "must have a formula or recipe")
 })
+
+test_that("cannot fit without a fit stage", {
+  workflow <- workflow()
+  workflow <- add_formula(workflow, mpg ~ cyl)
+
+  expect_error(fit(workflow, mtcars), "must have a model")
+})
+

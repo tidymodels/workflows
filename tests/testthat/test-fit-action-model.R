43562b7f88618ef754085fcab6d523ff82cea759
DavisVaughan <davis@rstudio.com>
2019-11-11 15:22:45 -0500

    Test formula override

--- tests/testthat/test-fit-action-model.R
+++ tests/testthat/test-fit-action-model.R
@@ -21,3 +21,23 @@ test_that("cannot add two models", {
 
   expect_error(add_model(workflow, mod), "`model` action has already been added")
 })
+
+test_that("can provide a model formula override", {
+  # disp is in the recipe, but excluded from the model formula
+  rec <- recipes::recipe(mpg ~ cyl + disp, mtcars)
+  rec <- recipes::step_center(rec, cyl)
+
+  mod <- parsnip::linear_reg()
+  mod <- parsnip::set_engine(mod, "lm")
+
+  workflow <- workflow()
+  workflow <- add_recipe(workflow, rec)
+  workflow <- add_model(workflow, mod, formula = mpg ~ cyl)
+
+  result <- fit(workflow, mtcars)
+
+  expect_equal(
+    c("(Intercept)", "cyl"),
+    names(result$fit$fit$fit$coefficients)
+  )
+})

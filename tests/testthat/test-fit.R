f82a386ba9312df5f79b2890a64315e5cc1ea5ba
DavisVaughan <davis@rstudio.com>
2019-11-11 15:13:46 -0500

    Ensure formula/recipe exists

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -1,4 +1,4 @@
-test_that("Missing `data` argument has a nice error", {
+test_that("missing `data` argument has a nice error", {
   mod <- parsnip::linear_reg()
   mod <- parsnip::set_engine(mod, "lm")
 
@@ -7,4 +7,14 @@ test_that("Missing `data` argument has a nice error", {
   workflow <- add_model(workflow, mod)
 
   expect_error(fit(workflow), "`data` must be provided to fit a workflow")
+})
+
+test_that("cannot fit without a pre stage", {
+  mod <- parsnip::linear_reg()
+  mod <- parsnip::set_engine(mod, "lm")
+
+  workflow <- workflow()
+  workflow <- add_model(workflow, mod)
+
+  expect_error(fit(workflow, mtcars), "must have a formula or recipe")
 })

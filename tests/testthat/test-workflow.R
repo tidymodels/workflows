a59e50f9d75e2f37463445bb2cd36a415a67dd4e
DavisVaughan <davis@rstudio.com>
2019-11-11 15:11:49 -0500

    Round out `workflow()` tests

--- tests/testthat/test-workflow.R
+++ tests/testthat/test-workflow.R
@@ -15,3 +15,20 @@ test_that("can create a basic workflow", {
 
   expect_equal(workflow$post$actions, list())
 })
+
+test_that("workflow must be the first argument when adding actions", {
+  rec <- recipes::recipe(mpg ~ cyl, mtcars)
+  mod <- parsnip::linear_reg()
+
+  expect_error(add_formula(1, mpg ~ cyl), "must be a workflow")
+  expect_error(add_recipe(1, rec), "must be a workflow")
+  expect_error(add_model(1, mod), "must be a workflow")
+})
+
+test_that("constructor validates input", {
+  expect_error(new_workflow(pre = 1), "must be a `stage`")
+  expect_error(new_workflow(fit = 1), "must be a `stage`")
+  expect_error(new_workflow(post = 1), "must be a `stage`")
+
+  expect_error(new_workflow(run = 1), "must be a single logical value")
+})

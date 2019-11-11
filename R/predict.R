0c30a99aaaab17c4706e8abee291af58f54c530a
DavisVaughan <davis@rstudio.com>
2019-11-11 14:26:33 -0500

    Alias with `workflow` for clarity

--- R/predict.R
+++ R/predict.R
@@ -1,14 +1,16 @@
 #' @export
 predict.workflow <- function(object, new_data, type = NULL, opts = list(), ...) {
-  if (!object$run) {
+  workflow <- object
+
+  if (!workflow$run) {
     abort("Workflow has not yet been fit. Do you need to call `fit()`?")
   }
 
-  blueprint <- object$pre$mold$blueprint
+  blueprint <- workflow$pre$mold$blueprint
   forged <- hardhat::forge(new_data, blueprint)
   new_data <- forged$predictors
 
-  fit <- object$fit$fit
+  fit <- workflow$fit$fit
 
   predict(fit, new_data, type = type, opts = opts, ...)
 }

08efb66b82a2660f3fdd324875bddd6097df9724
topepo <mxkuhn@gmail.com>
2019-11-25 16:36:15 -0500

    stackoverflow data set

--- R/data.R
+++ R/data.R
@@ -14,3 +14,21 @@
 #' @examples
 #' data(bivariate)
 NULL
+
+
+#' Annual Stack Overflow Developer Survey Data
+#'
+#' @details These data are a collection of 5,594 data points collected on
+#' developers. These data could be used to try to predict who works remotely
+#' (as used in the source listed below).
+#'
+#' @name stackoverflow
+#' @aliases stackoverflow
+#' @docType data
+#' @return \item{stackoverflow}{a tibble}
+#'
+#' @source https://supervised-ml-course.netlify.com/chapter2
+#' @keywords datasets
+#' @examples
+#' data(stackoverflow)
+NULL

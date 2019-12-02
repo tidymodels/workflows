4d4200b4b624bf46f9cc867d95a190433e9ea429
DavisVaughan <davis@rstudio.com>
2019-12-02 17:16:21 -0500

    Test that blueprint is passed on to `predict()`

--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -93,3 +93,27 @@ test_that("`new_data` must have all of the original predictors", {
 
   expect_error(predict(fit_workflow, cars_no_cyl), "missing: 'cyl'")
 })
+
+test_that("blueprint will get passed on to hardhat::forge()", {
+  mod <- parsnip::linear_reg()
+  mod <- parsnip::set_engine(mod, "lm")
+
+  # Pass formula explicitly to keep `lm()` from auto-generating an intercept
+  workflow <- workflow()
+  workflow <- add_model(workflow, mod, formula = mpg ~ . + 0)
+
+  blueprint_no_intercept <- hardhat::default_formula_blueprint(intercept = FALSE)
+  workflow_no_intercept <- add_formula(workflow, mpg ~ hp + disp, blueprint = blueprint_no_intercept)
+  fit_no_intercept <- fit(workflow_no_intercept, mtcars)
+  prediction_no_intercept <- predict(fit_no_intercept, mtcars)
+
+  blueprint_with_intercept <- hardhat::default_formula_blueprint(intercept = TRUE)
+  workflow_with_intercept <- add_formula(workflow, mpg ~ hp + disp, blueprint = blueprint_with_intercept)
+  fit_with_intercept <- fit(workflow_with_intercept, mtcars)
+  prediction_with_intercept <- predict(fit_with_intercept, mtcars)
+
+  expect_false(fit_no_intercept$pre$mold$blueprint$intercept)
+  expect_true(fit_with_intercept$pre$mold$blueprint$intercept)
+
+  expect_false(identical(prediction_with_intercept, prediction_no_intercept))
+})

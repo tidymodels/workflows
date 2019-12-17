f67b003998b28bfa9299ba386e7992f9ef9c8323
DavisVaughan <davis@rstudio.com>
2019-12-17 16:01:01 -0500

    Add tests for removing/updating a recipe

--- tests/testthat/test-pre-action-recipe.R
+++ tests/testthat/test-pre-action-recipe.R
@@ -20,6 +20,66 @@ test_that("cannot add a recipe if a formula already exists", {
   expect_error(add_recipe(workflow, rec), "cannot be added when a formula already exists")
 })
 
+test_that("remove a recipe", {
+  rec <- recipes::recipe(mpg ~ cyl, mtcars)
+
+  workflow_no_recipe <- workflow()
+  workflow_with_recipe <- add_recipe(workflow_no_recipe, rec)
+  workflow_removed_recipe <- remove_recipe(workflow_with_recipe)
+
+  expect_equal(workflow_no_recipe$pre, workflow_removed_recipe$pre)
+})
+
+test_that("remove a recipe after model fit", {
+  lm_model <- parsnip::linear_reg()
+  lm_model <- parsnip::set_engine(lm_model, "lm")
+
+  rec <- recipes::recipe(mpg ~ cyl, mtcars)
+
+  workflow_no_recipe <- workflow()
+  workflow_no_recipe <- add_model(workflow_no_recipe, lm_model)
+
+  workflow_with_recipe  <- add_recipe(workflow_no_recipe, rec)
+  workflow_with_recipe <- fit(workflow_with_recipe, data = mtcars)
+
+  workflow_removed_recipe <- remove_recipe(workflow_with_recipe)
+
+  expect_equal(workflow_no_recipe$pre, workflow_removed_recipe$pre)
+})
+
+test_that("update a recipe", {
+  rec <- recipes::recipe(mpg ~ cyl, mtcars)
+  rec2 <- recipes::recipe(mpg ~ disp, mtcars)
+
+  workflow <- workflow()
+  workflow <- add_recipe(workflow, rec)
+  workflow <- update_recipe(workflow, rec2)
+
+  expect_equal(workflow$pre$actions$recipe$recipe, rec2)
+})
+
+test_that("update a recipe after model fit", {
+  rec <- recipes::recipe(mpg ~ cyl, mtcars)
+  rec2 <- recipes::recipe(mpg ~ disp, mtcars)
+
+  lm_model <- parsnip::linear_reg()
+  lm_model <- parsnip::set_engine(lm_model, "lm")
+
+  workflow <- workflow()
+  workflow <- add_model(workflow, lm_model)
+  workflow <- add_recipe(workflow, rec)
+
+  workflow <- fit(workflow, data = mtcars)
+
+  # Should clear fitted model
+  workflow <- update_recipe(workflow, rec2)
+
+  expect_equal(workflow$pre$actions$recipe$recipe, rec2)
+
+  expect_equal(workflow$fit$actions$model$spec, lm_model)
+  expect_null(workflow$pre$mold)
+})
+
 test_that("recipe is prepped upon `fit()`", {
   rec <- recipes::recipe(mpg ~ cyl, mtcars)
   rec <- recipes::step_center(rec, cyl)

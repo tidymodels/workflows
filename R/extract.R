6720bbd781e87c22a50df31d11a77faee5cc0309
Max Kuhn <mxkuhn@gmail.com>
2021-06-25 16:35:07 -0400

    notes about model extraction

--- R/extract.R
+++ R/extract.R
@@ -29,6 +29,26 @@
 #' @param ... Not currently used.
 #' @details
 #' These functions supersede the `pull_*()` functions.
+#'
+#' Extracting the underlying fit objects can be helpful for describing the
+#'  model via `print()`, `summarize()`, `plot()`, and so on.
+#'
+#' However, users should not invoke the `predict()` method on an extracted
+#'  model. There may be preprocessing operations that `workflows` has executed on
+#'  the data prior to giving it to the model. Bypassing these can lead to errors
+#'  or silently generating incorrect predictions.
+#'
+#' *Good*:
+#' ```r
+#'    workflow_fit %>% predict(new_data)
+#' ```
+#'
+#' *Bad*:
+#' ```r
+#'    workflow_fit %>% extract_fit_engine()  %>% predict(new_data)
+#'    # or
+#'    workflow_fit %>% extract_fit_parsnip() %>% predict(new_data)
+#' ```
 #' @return
 #' The extracted value from the workflow, `x`, as described in the description
 #' section.

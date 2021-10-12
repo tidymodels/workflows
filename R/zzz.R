f872528a55005f6164240ec91f068234095ccf9a
Davis Vaughan <davis@rstudio.com>
2021-10-12 10:03:50 -0400

    Unconditionally register method for `generics::required_pkgs()` (#124)
    * Unconditionally register method for `generics::required_pkgs()`
    
    Many tune versions have passed since this move was done
    
    * Remove conditional skips on tune
    
    * Rename `tune.R` -> `generics.R`

--- R/zzz.R
+++ R/zzz.R
@@ -7,20 +7,7 @@
   vctrs::s3_register("butcher::axe_env", "workflow")
   vctrs::s3_register("butcher::axe_fitted", "workflow")
 
-  # Can't use `rlang::is_installed()` at all, as that doesn't work when
-  # called from `.onLoad()` for some reason. Instead, rely on `packageVersion()`
-  # erroring when the package isn't installed.
-  has_at_least_version <- function(pkg, version) {
-    tryCatch(
-      expr = utils::packageVersion(pkg) >= version,
-      error = function(cnd) FALSE
-    )
-  }
-
-  if (has_at_least_version("tune", "0.1.3.9001")) {
-    # `required_pkgs.workflow()` moved from tune to workflows
-    vctrs::s3_register("generics::required_pkgs", "workflow", required_pkgs_workflow)
-  }
+  vctrs::s3_register("generics::required_pkgs", "workflow")
 }
 
 # nocov end

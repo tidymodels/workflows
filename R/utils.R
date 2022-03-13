c3ddd7747e7ad6abf67c53b9442375b326b69518
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-03-12 20:04:42 -0800

    update is_model_fit and is_model_spec to work with celery objects

--- R/utils.R
+++ R/utils.R
@@ -11,11 +11,11 @@ glubort <- function(..., .sep = "", .envir = caller_env(), .call = .envir) {
 }
 
 is_model_fit <- function(x) {
-  inherits(x, "model_fit")
+  inherits(x, "model_fit") || inherits(x, "cluster_fit")
 }
 
 is_model_spec <- function(x) {
-  inherits(x, "model_spec")
+  inherits(x, "model_spec") || inherits(x, "cluster_spec")
 }
 
 validate_recipes_available <- function(..., call = caller_env()) {

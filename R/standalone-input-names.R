34211475fb0c9c92d5ac475ca092c93cbbf0055c
topepo <mxkuhn@gmail.com>
2024-01-29 13:29:07 -0500

    changes based on reviewer feedback

--- R/standalone-input-names.R
+++ R/standalone-input-names.R
@@ -1,11 +1,10 @@
 # ---
 # repo: tidymodels/workflows
 # file: standalone-input-names.R
-# last-updated: 2024-01-21
+# last-updated: 2024-01-291
 # license: https://unlicense.org
+# requires: cli, rlang
 # ---
-
-# secret gist at: https://gist.github.com/topepo/17d51cafcd0ac8dff0552198d6aeadbf
 
 # This file provides a portable set of helper functions for determining the
 # names of the predictor columns used as inputs into a workflow.
@@ -13,30 +12,21 @@
 # ## Changelog
 # 2024-01-21
 # * First version
+# 2024-01-29
+# * Changes after PR review
+
+# nocov start
 
 # ------------------------------------------------------------------------------
+# Primary functions
 
-check_workflow_fit <- function(x) {
-  if (!x$trained) {
-    stop("The workflow should be trained.")
-  }
-  invisible(NULL)
-}
+# @param x A _fitted_ workflow or recipe.
+# @param call An environment indicating where the top-level function was invoked
+# to print out better errors.
+# @return A character vector of sorted columns names.
 
-check_recipe_fit <- function(x) {
-  is_trained <- vapply(x$steps, function(x) x$trained, logical(1))
-  if (!all(is_trained)) {
-    stop("All recipe steps should be trained.")
-  }
-  invisible(NULL)
-}
-
-blueprint_ptype <- function(x) {
-  names(x$pre$mold$blueprint$ptypes$predictors)
-}
-
-.get_input_predictors_workflow <- function(x, ...) {
-  check_workflow_fit(x)
+.get_input_predictors_workflow <- function(x, ..., call = rlang::current_env()) {
+  check_workflow_fit(x, call = call)
   # We can get the columns that are inputs to the recipe but some of these may
   # not be predictors. We'll interrogate the recipe and pull out the current
   # predictor names from the original input
@@ -50,15 +40,44 @@ blueprint_ptype <- function(x) {
   sort(unique(res))
 }
 
-is_predictor_role <- function(x) {
-  vapply(x$role, function(x) any(x == "predictor"), logical(1))
-}
-
-.get_input_predictors_recipe <- function(x, ...) {
-  check_recipe_fit(x)
+.get_input_predictors_recipe <- function(x, ..., call = rlang::current_env()) {
+  check_recipe_fit(x, call = call)
   var_info <- x$last_term_info
 
   keep_rows <- var_info$source == "original" & is_predictor_role(var_info)
   var_info <- var_info[keep_rows,]
   var_info$variable
 }
+
+.get_input_outcome_workflow <- function(x) {
+  check_workflow_fit(x)
+  names(x$pre$mold$blueprint$ptypes$outcomes)
+}
+
+# ------------------------------------------------------------------------------
+# Helper functions
+
+check_workflow_fit <- function(x, call) {
+  if (!x$trained) {
+    cli::cli_abort("The workflow should be trained.", call = call)
+  }
+  invisible(NULL)
+}
+
+check_recipe_fit <- function(x, call) {
+  is_trained <- vapply(x$steps, function(x) x$trained, logical(1))
+  if (!all(is_trained)) {
+    cli::cli_abort("All recipe steps should be trained.", call = call)
+  }
+  invisible(NULL)
+}
+
+blueprint_ptype <- function(x) {
+  names(x$pre$mold$blueprint$ptypes$predictors)
+}
+
+is_predictor_role <- function(x) {
+  vapply(x$role, function(x) any(x == "predictor"), logical(1))
+}
+
+# nocov end
--- tests/testthat/_snaps/input-names.md
+++ tests/testthat/_snaps/input-names.md
@@ -3,30 +3,30 @@
     Code
       workflows:::.get_input_predictors_workflow(workflow)
     Condition
-      Error in `check_workflow_fit()`:
-      ! The workflow should be trainined.
+      Error in `workflows:::.get_input_predictors_workflow()`:
+      ! The workflow should be trained.
 
 ---
 
     Code
       workflows:::.get_input_predictors_recipe(rec_with_id)
     Condition
-      Error in `check_recipe_fit()`:
-      ! All recipe steps should be trainined.
+      Error in `workflows:::.get_input_predictors_recipe()`:
+      ! All recipe steps should be trained.
 
 # get formula input column names
 
     Code
       workflows:::.get_input_predictors_workflow(workflow)
     Condition
-      Error in `check_workflow_fit()`:
-      ! The workflow should be trainined.
+      Error in `workflows:::.get_input_predictors_workflow()`:
+      ! The workflow should be trained.
 
 # get predictor input column names
 
     Code
       workflows:::.get_input_predictors_workflow(workflow)
     Condition
-      Error in `check_workflow_fit()`:
-      ! The workflow should be trainined.
+      Error in `workflows:::.get_input_predictors_workflow()`:
+      ! The workflow should be trained.
 

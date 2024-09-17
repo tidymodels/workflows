84b21c040af52e645cb1185d145d56810b933944
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-09-17 13:52:30 -0700

    xgboost -> glmnet

--- tests/testthat/test-sparsevctrs.R
+++ tests/testthat/test-sparsevctrs.R
@@ -1,5 +1,5 @@
 test_that("sparse tibble can be passed to `fit() - recipe", {
-  skip_if_not_installed("xgboost")
+  skip_if_not_installed("glmnet")
   
   hotel_data <- sparse_hotel_rates()
   hotel_data <- sparsevctrs::coerce_to_sparse_tibble(hotel_data)
@@ -8,9 +8,9 @@ test_that("sparse tibble can be passed to `fit() - recipe", {
   
   withr::local_options("sparsevctrs.verbose_materialize" = 3)
 
-  spec <- parsnip::boost_tree() %>%
+  spec <- parsnip::linear_reg(penalty = 0) %>%
     parsnip::set_mode("regression") %>%
-    parsnip::set_engine("xgboost")
+    parsnip::set_engine("glmnet")
 
   rec <- recipes::recipe(avg_price_per_room ~ ., data = hotel_data)
 
@@ -18,19 +18,19 @@ test_that("sparse tibble can be passed to `fit() - recipe", {
     add_recipe(rec) %>%
     add_model(spec)
   
-  expect_no_condition(wf_fit <- fit(wf_spec, hotel_data))
+  expect_no_error(wf_fit <- fit(wf_spec, hotel_data))
 })
 
 test_that("sparse tibble can be passed to `fit() - formula", {
-  skip_if_not_installed("xgboost")
+  skip_if_not_installed("glmnet")
   withr::local_options("sparsevctrs.verbose_materialize" = 3)
 
   hotel_data <- sparse_hotel_rates()
   hotel_data <- sparsevctrs::coerce_to_sparse_tibble(hotel_data)
 
-  spec <- parsnip::boost_tree() %>%
+  spec <- parsnip::linear_reg(penalty = 0) %>%
     parsnip::set_mode("regression") %>%
-    parsnip::set_engine("xgboost")
+    parsnip::set_engine("glmnet")
 
   wf_spec <- workflow() %>%
     add_formula(avg_price_per_room ~ .) %>%
@@ -43,7 +43,7 @@ test_that("sparse tibble can be passed to `fit() - formula", {
 })
 
 test_that("sparse tibble can be passed to `fit() - xy", {
-  skip_if_not_installed("xgboost")
+  skip_if_not_installed("glmnet")
 
   hotel_data <- sparse_hotel_rates()
   hotel_data <- sparsevctrs::coerce_to_sparse_tibble(hotel_data)
@@ -52,9 +52,9 @@ test_that("sparse tibble can be passed to `fit() - xy", {
   
   withr::local_options("sparsevctrs.verbose_materialize" = 3)
 
-  spec <- parsnip::boost_tree() %>%
+  spec <- parsnip::linear_reg(penalty = 0) %>%
     parsnip::set_mode("regression") %>%
-    parsnip::set_engine("xgboost")
+    parsnip::set_engine("glmnet")
 
   wf_spec <- workflow() %>%
     add_variables(avg_price_per_room, everything()) %>%

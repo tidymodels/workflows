19d07574394bbb6179ddf744315c18962f29d7af
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-09-20 15:49:27 -0700

    test that sparse matrices work with fit.workflow()

--- tests/testthat/_snaps/sparsevctrs.md
+++ tests/testthat/_snaps/sparsevctrs.md
@@ -6,3 +6,25 @@
       Error in `fit()`:
       ! Sparse data cannot be used with formula interface. Please use `add_recipe()` or `add_variables()` instead.
 
+# sparse matrices can be passed to `fit() - recipe
+
+    Code
+      wf_fit <- fit(wf_spec, hotel_data)
+    Output
+      sparsevctrs: Sparse vector materialized
+
+# sparse matrices can be passed to `fit() - formula
+
+    Code
+      wf_fit <- fit(wf_spec, hotel_data)
+    Condition
+      Error in `fit()`:
+      ! Sparse data cannot be used with formula interface. Please use `add_recipe()` or `add_variables()` instead.
+
+# sparse matrices can be passed to `fit() - xy
+
+    Code
+      wf_fit <- fit(wf_spec, hotel_data)
+    Output
+      sparsevctrs: Sparse vector materialized
+
--- tests/testthat/test-sparsevctrs.R
+++ tests/testthat/test-sparsevctrs.R
@@ -53,3 +53,61 @@ test_that("sparse tibble can be passed to `fit() - xy", {
 
   expect_no_error(wf_fit <- fit(wf_spec, hotel_data))
 })
+
+test_that("sparse matrices can be passed to `fit() - recipe", {
+  skip_if_not_installed("glmnet")
+  withr::local_options("sparsevctrs.verbose_materialize" = 1)
+
+  hotel_data <- sparse_hotel_rates()
+
+  spec <- parsnip::linear_reg(penalty = 0) %>%
+    parsnip::set_mode("regression") %>%
+    parsnip::set_engine("glmnet")
+
+  rec <- recipes::recipe(avg_price_per_room ~ ., data = hotel_data)
+
+  wf_spec <- workflow() %>%
+    add_recipe(rec) %>%
+    add_model(spec)
+
+  # We expect 1 materialization - the outcome
+  expect_snapshot(wf_fit <- fit(wf_spec, hotel_data))
+})
+
+test_that("sparse matrices can be passed to `fit() - formula", {
+  skip_if_not_installed("glmnet")
+  withr::local_options("sparsevctrs.verbose_materialize" = 3)
+
+  hotel_data <- sparse_hotel_rates()
+
+  spec <- parsnip::linear_reg(penalty = 0) %>%
+    parsnip::set_mode("regression") %>%
+    parsnip::set_engine("glmnet")
+
+  wf_spec <- workflow() %>%
+    add_formula(avg_price_per_room ~ .) %>%
+    add_model(spec)
+
+  expect_snapshot(
+    error = TRUE,
+    wf_fit <- fit(wf_spec, hotel_data)
+  )
+})
+
+test_that("sparse matrices can be passed to `fit() - xy", {
+  skip_if_not_installed("glmnet")
+  withr::local_options("sparsevctrs.verbose_materialize" = 1)
+
+  hotel_data <- sparse_hotel_rates()
+
+  spec <- parsnip::linear_reg(penalty = 0) %>%
+    parsnip::set_mode("regression") %>%
+    parsnip::set_engine("glmnet")
+
+  wf_spec <- workflow() %>%
+    add_variables(avg_price_per_room, everything()) %>%
+    add_model(spec)
+
+  # We expect 1 materialization - the outcome
+  expect_snapshot(wf_fit <- fit(wf_spec, hotel_data))
+})

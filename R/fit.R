09b63670e9ae543c34dc1423d4545ede208608f6
simonpcouch <simonpatrickcouch@gmail.com>
2025-02-18 12:56:06 -0600

    Revert "correct interpretation of `add_tailor(prop)` (#248)"
    This reverts commit dd254f8598ab27a14a9ba979abb580e9b3a18da2.

--- R/fit.R
+++ R/fit.R
@@ -96,12 +96,10 @@ make_inner_split <- function(object, data) {
       class = if (is.null(method)) "mc_split" else method
     )
 
-  # add_tailor(prop) is the proportion to train the postprocessor, while
-  # rsample::mc_cv(prop) is the proportion to train the model (#247)
   prop <- object$post$actions$tailor$prop
   rsample::inner_split(
     mocked_split,
-    list(prop = if (is.null(prop)) 2/3 else 1 - prop)
+    list(prop = if (is.null(prop)) 2/3 else prop)
   )
 }
 
